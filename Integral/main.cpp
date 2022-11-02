d24aa0d6fef9bb50b1968e4556ba8bfeaf51ef3f
Диков Александр <112902848+DikovAlexandr@users.noreply.github.com>
2022-11-02 17:18:45 +0300

    simpson and three eighths rewrited

--- Integral/main.cpp
+++ Integral/main.cpp
@@ -11,28 +11,32 @@ using namespace std;
 double f(double x) {
     // return (pow(x, 4) + 5 * pow(x, 3) + 2 * pow(x, 2) + 15 * x + 2);
     // return exp(-x);
-    return sin(x);
+    // return sin(x);
+    return pow(x, 4);
 }
 
 // Вторая производная
 double d2_f(double x) {
     // return (pow(x, 4) + 5 * pow(x, 3) + 2 * pow(x, 2) + 15 * x + 2);
     // return exp(-x);
-    return -sin(x);
+    // return -sin(x);
+    return 12 * pow(x, 2);
 }
 
 // Четвертая производная
 double d4_f(double x) {
     // return (pow(x, 4) + 5 * pow(x, 3) + 2 * pow(x, 2) + 15 * x + 2);
     // return exp(-x);
-    return sin(x);
+    // return sin(x);
+    return 24;
 }
 
 // Аналитический интеграл
 double analyticIntegral(double a, double b) {
     // return pow(b, 5) / 5 + 5 * pow(b, 4) / 4 + 2 * pow(b, 3) / 3 + 15 * pow(b, 2) / 2 + 2 * b;
     // return 1 - exp(-3);
-    return 2;
+    // return 2;
+    return 0.2;
 }
 
 // Максимум второй производной на промежутке интегрирования
@@ -93,6 +97,17 @@ double threeEighthSum(double a, double b, double h) {
     return integral;
 }
 
+// Новый метод трех восьмых
+double threeEighthSumNew(double a, double b, double n) {
+    double h = (b - a) / (n * 3.0);
+    double integral = 0, point;
+    for (int i = 0; i < n; ++i) {
+        point = a + i * h * 3.0;
+        integral += f(point) + 3.0 * f(point + h) + 3.0 * f(point + 2.0 * h) + f(point + 3.0 * h);
+    }
+    return integral * h * 3.0 / 8.0;
+}
+
 // Метод Симпсона
 double simpsonSum(double a, double b, double h) {
     double integral;
@@ -105,6 +120,17 @@ double simpsonSum(double a, double b, double h) {
     for (int i = 1; i <= n; i++)
         integral = integral + 2.0 / 6.0 * h * f(a + h * i);
     return integral;
+}
+
+// Новый метод Симпсона
+double simpsonSumNew(double a, double b, double n) {
+    double h = (b - a) / (n * 2.0);
+    double integral = 0, point;
+    for (int i = 0; i < n; ++i) {
+        point = a + i * h * 2.0;
+        integral += f(point) + 4.0 * f(point + h) + f(point + 2.0 * h);
+    }
+    return integral * h / 3.0;
 }
 
 //////////////////////////////
@@ -123,8 +149,8 @@ double trapezoidalAccuracy(double a, double b, double h) {
 }
 
 // Теоретическая погрешность метода трех восьмых
-double threeEighthsAccuracy(double a, double b, double h) {
-    return pow(h, 4) * (b - a) * max_d4_f(a, b, h) / 80.0;
+double threeEighthsAccuracy(double a, double b, double n, double h) {
+    return pow((b - a) / n, 5) * 3.0 / 8.0 * 24;
     // где max_d4_f это максимальная величина четвертой производной от f
 }
 
@@ -136,8 +162,9 @@ double simpsonAccuracy(double a, double b, double h) {
 
 int main() {
     double a = 0.0;
-    double b = M_PI;
-    double h = 0.001;
+    double b = 1.0;
+    double h = 0.1;
+    double n = 10;
 
     // Настройки точности вывода
     const auto digits = 10;
@@ -151,11 +178,11 @@ int main() {
     cout << "Trapezoidal sum " << trapezoidalSum(a, b, h) << " different from analytic "
          << fabs(analyticIntegral(a, b) - trapezoidalSum(a, b, h))
          << " meanwhile theoretical accuracy " << trapezoidalAccuracy(a, b, h) << endl;
-    cout << "Three eighths sum " << threeEighthSum(a, b, h) << " different from analytic "
-         << fabs(analyticIntegral(a, b) - threeEighthSum(a, b, h))
-         << " meanwhile theoretical accuracy " << threeEighthsAccuracy(a, b, h) << endl;
-    cout << "Simpson sum " << simpsonSum(a, b, h) << " different from analytic "
-         << fabs(analyticIntegral(a, b) - simpsonSum(a, b, h))
+    cout << "Three eighths sum " << threeEighthSumNew(a, b, n) << " different from analytic "
+         << fabs(analyticIntegral(a, b) - threeEighthSumNew(a, b, n))
+         << " meanwhile theoretical accuracy " << threeEighthsAccuracy(a, b, n, h) << endl;
+    cout << "Simpson sum " << simpsonSumNew(a, b, n) << " different from analytic "
+         << fabs(analyticIntegral(a, b) - simpsonSumNew(a, b, n))
          << " meanwhile theoretical accuracy " << simpsonAccuracy(a, b, h) << endl;
     cout << "Analytic result " << analyticIntegral(a, b) << endl;
     return 0;
